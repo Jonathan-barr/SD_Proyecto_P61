b93930e511885696d480270684b2b7dc82a1002c
Jonathan-barr <jonathan_barrionuevo@hotmail.com>
2023-01-24 18:44:30 -0500

    funciona GET y POST tenemos en server 3001

--- API_SERVIDOR/src/database.js
+++ API_SERVIDOR/src/database.js
@@ -1,48 +1,38 @@
 var MongoClient = require("mongodb").MongoClient;
-var url = 'mongodb://dbjc:27017/';
 
-MongoClient.connect(url, function (err, db) {
-  if (err) {
-    throw err;
-  }
-  var dbo = db.db("distribuidos");
-  exports.insertarProducto = function (obj) {
-    dbo.collection("productos").insertOne(obj, function (err, res) {
-      if (err) throw err;
-      console.log("1 document inserted");
-      db.close();
-    });
-  };
-  dbo
-    .collection("productos")
-    .find({})
-    .toArray(function (err, result) {
-      if (err) throw err;
-      console.log("llegamos a la funcion se mamo");
-      exports.consultarProductos = function () {
-        console.log("este es el resultado: " + "prueba" + result);
-        return result;
-      };
-      db.close();
-    });
+const url = 'mongodb://dbjc:27017';
+const client = new MongoClient(url);
 
-  exports.querys = function () {
-    dbo
-      .collection("customers")
-      .find(query)
-      .toArray(function (err, result) {
-        if (err) throw err;
-        exports.consultarQuery = function () {
-          return result;
-        };
-        db.close();
-      });
-  };
+const dbName = 'distribuidos';
 
-  exports.borrarProducto = function (obj) {
-    dbo.collection("customers").deleteOne(myquery, function (err, obj) {
-      if (err) throw err;
-      db.close();
-    });
-  };
-});
+exports.consultarProductos = async function () {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('productos');
+  const findResult = await collection.find({}).toArray();
+
+  return findResult;
+}
+
+exports.insertarProductos = async function (obj) {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('productos');
+  const insertResult = await collection.insertOne(obj);
+
+  return insertResult;
+}
+
+exports.eliminarProductos = async function (obj) {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('productos');
+  const deleteResult = await collection.deleteOne(obj);
+  return deleteResult;
+}
--- API_SERVIDOR/src/index.js
+++ API_SERVIDOR/src/index.js
@@ -1,29 +1,62 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const morgan=require('morgan');
-const db = require('./database.js')
- 
+const morgan = require("morgan");
+const db = require("./database.js");
+
+var MongoClient = require("mongodb").MongoClient;
+
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+
+const cors = require("cors");
+
+app.use(
+    cors({
+      origin: "*",
+      optionsSuccessStatus: 200,
+      methods: "GET, PUT, POST"
+    })
+  );
+
+app.options('*',cors());
+
 //Configuraciones
-app.set('port', process.env.PORT || 3000);
-app.set('json spaces', 2)
- 
+app.set("port", process.env.PORT || 3000);
+app.set("json spaces", 2);
+
 //Middleware
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended:false}));
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
- 
+
 //Nuestro primer WS Get
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'pages/index.html')); 
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "pages/index.html"));
+});
 
-app.get('/datos', (req, res) => {
-    res.json(db.consultarProductos()); 
-})
+app.get("/datos", (req, res) => {
+  db.consultarProductos()
+    .then((val) => res.json(val))
+    .catch((err) => console.error(err))
+    .finally(() => client.close());
+});
 
- 
+app.post("/insertDatos", (req, res) => {
+  db.insertarProductos(req.body)
+    .then((val) => res.json(val))
+    .catch((err) => console.error(err))
+    .finally(() => client.close());
+});
+
+app.post("/eliminarDatos", (req, res) => {
+    db.insertarProductos(req.body)
+      .then((val) => res.json(val))
+      .catch((err) => console.error(err))
+      .finally(() => client.close());
+  });
+
 //Iniciando el servidor
-app.listen(app.get('port'),()=>{
-    console.log(`Server listening on port ${app.get('port')}`);
+app.listen(app.get("port"), () => {
+  console.log(`Server listening on port ${app.get("port")}`);
 });
